a1a97380c8d08ac7df7aca4d11f26bf6483fd863
Jose Arenado <vidalstat@gmail.com>
2023-03-23 16:32:34 -0400

    feat: add hex converter

--- lua/nvim-color-picker/init.lua
+++ lua/nvim-color-picker/init.lua
@@ -1,7 +1,28 @@
-local M = {}
-
-function M.do_something()
-  print('Hello world, color picker')
+function map(mode, lhs, rhs, opts)
+  local options = { noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
-return M
+function grab_color()
+  local handle = io.popen('zenity --color-selection')
+  local result = handle:read("*a")
+  handle:close()
+  print(result)
+  vim.api.nvim_set_current_line(result)
+end
+
+function ConvertToHex(color) -- Converts RGB colors to HEX
+	local hex = {}
+
+	for rgb in color:gmatch('%d+') do
+		table.insert(hex, ('%02X'):format(tonumber(rgb)))
+	end
+
+	return table.concat(hex)
+end
+
+--map("n", "<C-G>", ":lua os.execute('zenity --color-selection')<cr>", { silent = true })
+map("n", "<C-G>", ":lua grab_color()", { silent = true })
